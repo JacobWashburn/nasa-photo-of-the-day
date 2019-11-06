31fdeb22835c7935929eef191ff9a23a94618174
Jacob Washburn <jacobrwashburn@gmail.com>
2019-11-06 16:55:06 -0700

    small footer built out

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return(
+        <div className='footer'>
+            <p>This page was made by Jacob Washburn</p>
+        </div>
+    )
+}
