e82c77c2f3372e3df04f66b47b80233a4e7ae35a
Jacob Washburn <jacobrwashburn@gmail.com>
2019-12-19 15:13:38 -0700

    some stuff

--- src/App.js
+++ src/App.js
@@ -13,18 +13,42 @@ function App() {
     let month = String(getToday.getMonth() + 1).padStart(2, '0'); //January is 0!
     let year = getToday.getFullYear();
 
-    const today = year + '-' + month + '-' + day
+    const today = year + '-' + month + '-' + getDay
     const [dayChange, setDayChange] = useState(year + '-' + month + '-' + day)
 
     const [image, setImage] = useState([]);
     const [date, setDate] = useState(today);
+    console.log('the date is',date)
+
+    
+    // let another = (param) => {
+    //     console.log('woot', param)
+    //     setDay(param)
+    //     console.log({day})
+    //     setDayChange(year + '-' + month + '-' + day)
+    //     console.log({dayChange})
+    //     setDate(dayChange)
+    //     console.log({date})
+    //     axios.get(`https://api.nasa.gov/planetary/apod?api_key=IhKxDSJTAJjc89d4fOXzVpvFX7iR1E1KerB5l101&date=${date}`)
+    //         .then(response => {
+    //             setImage(response.data)
+    //             console.log('axios call', response)
+    //         })
+    //         .catch(error => {
+    //             console.log(error.message)
+    //         })
+    // };
 
     useEffect(()=>{
         setDayChange(year + '-' + month + '-' + day)
-        setDate(dayChange)
     },[day])
 
+    useEffect(()=>{
+        setDate(dayChange)
+    },[dayChange])
+
     useEffect(() => {
+        // another(day)
         axios.get(`https://api.nasa.gov/planetary/apod?api_key=IhKxDSJTAJjc89d4fOXzVpvFX7iR1E1KerB5l101&date=${date}`)
             .then(response => {
                 setImage(response.data)
--- src/components/MainBody/Card/Buttons/ForwardButton.js
+++ src/components/MainBody/Card/Buttons/ForwardButton.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Button from './Button'
 
 export default function ForwardButton(props){
+    console.log('forward props',props)
     const day = Number(props.day) + 1;
     const dayNumToString = String(day).padStart(2, '0');
     if (props.date === props.today) {
